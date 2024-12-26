dd51c7edbeb345be761d162fc78094688ffda521
Ahmed Shehab <ahmed.shehab06@eng-st.cu.edu.eg>
2024-12-26 05:39:24 +0000

    Code Refactoring

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,15 +2,15 @@ const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
 class AppController {
-  static getStatus (req, res) {
+  static getStatus(req, res) {
     return res.status(200).json(
-      { redis: redisClient.isAlive(), db: dbClient.isAlive() }
+      { redis: redisClient.isAlive(), db: dbClient.isAlive() },
     );
   }
 
-  static async getStats (req, res) {
+  static async getStats(req, res) {
     return res.status(200).json(
-      { users: await dbClient.nbUsers(), files: await dbClient.nbFiles() }
+      { users: await dbClient.nbUsers(), files: await dbClient.nbFiles() },
     );
   }
 }
